70175c98f52afc3fd31d15916e0c1dcb0d7bf885
Ruben Brito <rbenfsabrito@outlook.com>
2019-12-16 02:42:58 +0000

    Hello World

---
+++ app/src/main/java/com/rb/fgx/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rb.fgx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
